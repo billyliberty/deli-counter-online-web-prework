db29ca08f02345af8a95652cfbce6c0553329c5a
Joshua Crum <joshua.crum@gmail.com>
2019-04-30 18:59:53 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,13 +1,13 @@
 def line(place_in_line)
   line_array = []
-  if place_in_line.length == 0
-    puts "The line is currently empty."
-  else
+  if
    place_in_line.each.with_index(1) do |name, index|
      line_array.push("#{index}. #{name}")
    end
    puts "The line is currently: #{line_array.join(" ")}"
  end
+ else 
+   puts "The line is currently empty."
 end
 
 def take_a_number(katz_deli, name)
