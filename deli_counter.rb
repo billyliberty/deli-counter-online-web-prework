52f0991fe337a9313657fd68978432dedbb6ca80
Joshua Crum <joshua.crum@gmail.com>
2019-04-30 19:00:34 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,8 +5,9 @@ def line(place_in_line)
      line_array.push("#{index}. #{name}")
    end
    puts "The line is currently: #{line_array.join(" ")}"
- end
- else
+  end
+ else 
+   if place_in_line.length == 0
    puts "The line is currently empty."
 end
 
