dff056ee0a98ea42f18cd34da1b669dd30751f1a
Joshua Crum <joshua.crum@gmail.com>
2019-04-30 19:04:13 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,7 @@
 def line(place_in_line)
   line_array = []
   if place_in_line.length == 0
-    puts "The line is currently empty"
+    puts "The line is currently empty."
   else
    place_in_line.each.with_index(1) do |name, index|
      line_array.push("#{index}. #{name}")
