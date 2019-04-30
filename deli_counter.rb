f6d67caaa11795297f6d66d40506186ab73e6d7d
Joshua Crum <joshua.crum@gmail.com>
2019-04-30 17:33:24 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,12 @@
-# Write your code here.
+def line(place_in_line)
+  place_in_line_array = []
+  if place_in_line.length == 0
+    return "The line is currently empty."
+end
+
+def take_a_number
+end
+
+def now_serving
+end
+
