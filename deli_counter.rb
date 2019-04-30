a201ccd8003832143a1ae630eb7ff1874adbddd0
Joshua Crum <joshua.crum@gmail.com>
2019-04-30 17:57:24 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -8,7 +8,7 @@ end
 
 def take_a_number(katz_deli, name)
   katz_deli.push(name)
-  puts "Welcome #{name}. You are #{katz_deli.length} in line."
+  puts "Welcome #{name}. You are number #{katz_deli.length} in line."
 end
 
 def now_serving(number_in_line)
